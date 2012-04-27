3deb7522704599d6da939e96e5415c0569db6045
Nathan McKinley <ndm25@case.edu>
2012-04-27 18:20:50 -0400

    arduino code added, directory tree refactored
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

---
+++ Arduino/sketch_apr19b.ino
@@ -0,0 +1,46 @@
+int inPin;
+int outPin;
+int recentIn[4];
+boolean sent;
+
+void setup()
+{
+  // alter based on your own setup
+  inPin = 9;
+  outPin = 5;
+
+
+  Serial.begin(9600);
+  pinMode(inPin, INPUT);
+  pinMode(outPin, OUTPUT);
+  digitalWrite(inPin, HIGH);
+  recentIn[0] = HIGH;
+  recentIn[1] = HIGH;
+  recentIn[2] = HIGH;
+  recentIn[3] = HIGH;
+  sent = false;
+}
+
+void loop()
+{
+  digitalWrite(outPin, LOW);
+  // slide everything back a bit
+  recentIn[0] = recentIn[1];
+  recentIn[1] = recentIn[2];
+  recentIn[2] = recentIn[3];
+  recentIn[3] = digitalRead(inPin);
+  // confirm that we're not seeing a fluctuation
+  // basically a low-pass filter
+  if (recentIn[0] + recentIn[1] + recentIn[2] + recentIn[3] == LOW)
+  {
+    if (!sent)
+    {
+      Serial.println("Input Received");
+      sent = true;
+    }
+  }
+  else if (recentIn[0] + recentIn[1] + recentIn[2] + recentIn[3] == 4 * HIGH)
+  {
+    sent = false;
+  }
+}
